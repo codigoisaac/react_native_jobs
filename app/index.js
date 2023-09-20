5741ec823e3e55bcbed200814b9714f047441973
IsaacMuniz <isaacmuniz@protonmail.com>
2023-09-20 07:01:17 -0300

    Basic initial home layout

--- app/index.js
+++ app/index.js
@@ -1,10 +1,40 @@
-import { Text, View } from "react-native";
+import { Stack } from "expo-router";
+import { SafeAreaView, ScrollView, View } from "react-native";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome
+} from "../components";
+import { COLORS, icons, images, SIZES } from "../constants";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Hello Mobile Development :$</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerTitle: "",
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="60%" />
+          )
+        }}
+      />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+
+          <Popularjobs />
+
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
--- constants/theme.js
+++ constants/theme.js
@@ -12,7 +12,7 @@ const COLORS = {
 
 const FONT = {
   regular: "DMRegular",
-  medium: "DMMedium",
+  medium: "Roboto",
   bold: "DMBold"
 };
 
