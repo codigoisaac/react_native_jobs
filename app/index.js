cef9cfc921662e728b083b27b4e9b2d264008507
IsaacMuniz <isaacmuniz@protonmail.com>
2023-09-20 04:24:03 -0300

    Hello Mobile Development :}

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Hello Mobile Development :$</Text>
+    </View>
+  );
+};
+
+export default Home;
