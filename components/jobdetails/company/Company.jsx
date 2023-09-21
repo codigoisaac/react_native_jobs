0514ea2cabc776643a52adf9984244b88123b6f2
IsaacMuniz <isaacmuniz@protonmail.com>
2023-09-21 03:27:09 -0300

    Job page

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -13,7 +13,7 @@ const Company = ({ companyLogo, jobTitle, companyName, location }) => {
           source={{
             uri: checkImageURL(companyLogo)
               ? companyLogo
-              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg"
           }}
           style={styles.logoImage}
         />
@@ -25,12 +25,14 @@ const Company = ({ companyLogo, jobTitle, companyName, location }) => {
 
       <View style={styles.companyInfoBox}>
         <Text style={styles.companyName}>{companyName} / </Text>
+
         <View style={styles.locationBox}>
           <Image
             source={icons.location}
-            resizeMode='contain'
+            resizeMode="contain"
             style={styles.locationImage}
           />
+
           <Text style={styles.locationName}>{location}</Text>
         </View>
       </View>
